bcbac552895ffab2b8a28ed9dc4e7846273424c8
Mantu Kumar <amritguru.work@gmail.com>
2026-02-18 14:45:39 +0530

    live links updated

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL =  "https://linkedin-clone-kb5b.onrender.com/" //"http://localhost:9080"
+export const BASE_URL =  "https://linkedin-clone-kb5b.onrender.com" //"http://localhost:9080"
 
 
 export const clientServer = axios.create({
